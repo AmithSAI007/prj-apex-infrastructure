6be733cc99c60972fecefd65344557def61bff7a
Amith Sai <amith@Amiths-Air.lan>
2026-01-16 12:21:13 +0530

    chore: dummy commit

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -1,14 +1,13 @@
 resource "google_storage_bucket" "apex_dev_gcs_raw_videos" {
-  name     = var.raw_videos_bucket_name
-  location =  var.bucket_location
+  name          = var.raw_videos_bucket_name
+  location      = var.bucket_location
   force_destroy = true
-  project = var.project_id
-  
+  project       = var.project_id
 }
 
 resource "google_storage_bucket" "apex_dev_gcs_processed_videos" {
-  name     = var.processed_videos_bucket_name
-  location =  var.bucket_location
+  name          = var.processed_videos_bucket_name
+  location      = var.bucket_location
   force_destroy = true
-  project = var.project_id
+  project       = var.project_id
 }
