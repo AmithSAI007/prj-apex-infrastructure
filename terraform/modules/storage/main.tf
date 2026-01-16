797ce05d80806cee06c198efbd6c6dba15d06a42
Amith Sai <amith@Amiths-Air.lan>
2026-01-16 12:33:45 +0530

    feat: added uniform bucket level access

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -1,13 +1,15 @@
 resource "google_storage_bucket" "apex_dev_gcs_raw_videos" {
-  name          = var.raw_videos_bucket_name
-  location      = var.bucket_location
-  force_destroy = true
-  project       = var.project_id
+  name                        = var.raw_videos_bucket_name
+  location                    = var.bucket_location
+  force_destroy               = true
+  project                     = var.project_id
+  uniform_bucket_level_access = true
 }
 
 resource "google_storage_bucket" "apex_dev_gcs_processed_videos" {
-  name          = var.processed_videos_bucket_name
-  location      = var.bucket_location
-  force_destroy = true
-  project       = var.project_id
+  name                        = var.processed_videos_bucket_name
+  location                    = var.bucket_location
+  force_destroy               = true
+  project                     = var.project_id
+  uniform_bucket_level_access = true
 }
