2b8a6ec6b6359aaecb34096ea8b1daab0480b81c
Amith Sai <amith@Amiths-MacBook-Air.local>
2026-01-26 16:53:26 +0530

    chore: dummy commit

--- terraform/shared/variables.tf
+++ terraform/shared/variables.tf
@@ -3,7 +3,7 @@ variable "project_id" {
   description = "The unique identifier for the GCP project for resource organization and billing."
   validation {
     condition     = length(var.project_id) > 0
-    error_message = "The project_id must not be empty."
+    error_message = "The project_id must not be empty"
   }
 }
 
